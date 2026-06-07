a563a724bc6f08c31d6475a002fdb0ab7d33d0a0
Stephanie <stephaniecharles147@gmail.com>
2026-06-07 15:35:16 +0100

    feat: initialize frontend with React, vite , axios, react router dom, eslint, prettier, tan stack query, remixicon and Tailwind CSS
    - Integrated axios and React Query for data fetching.
    - Configured Axios for API requests with token management.
    - Set up Tailwind CSS for styling.

---
+++ frontend/eslint.config.js
@@ -0,0 +1,33 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import { defineConfig, globalIgnores } from 'eslint/config';
+
+export default defineConfig([
+  globalIgnores(['dist', 'node_modules', 'coverage', 'build']),
+
+  {
+    files: ['**/*.{js,jsx}'],
+
+    extends: [
+      js.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+    ],
+
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: globals.browser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+  },
+
+  eslintPluginPrettierRecommended,
+]);
---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <p> This page is blank</p>
+    </>
+  )
+}
+
+export default App
